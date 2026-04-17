ead24c763c3297fc53fd392b41a425e269f15a50
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-17 11:24:30 +0600

    a

---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart ';
+
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  late bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked,
+          toggleCheckboxState: (bool checkboxState) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+          }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox(
+      {required this.checkboxState, required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      // onChanged: toggleCheckboxState,
+      onChanged: (value) => toggleCheckboxState(value),
+    );
+  }
+}
